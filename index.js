c9380ec7af9b943cebfc1ad2fc2a24f2318105a7
Bjorn Sveia <bjorn@radalabs.net>
2021-06-07 10:31:05 +0200

    Update

--- index.js
+++ index.js
@@ -25,13 +25,25 @@ function show() {
       </audio>
 
       <div class="header">Beat Master</div>
-      <div class="s1" onclick="document.getElementById('myTune1').play()"></div>
-      <div class="s2" onclick="document.getElementById('myTune2').play()"></div>
-      <div class="s3" onclick="document.getElementById('myTune3').play()"></div>
-      <div class="s4" onclick="document.getElementById('myTune4').play()"></div>
-      <div class="s5" onclick="document.getElementById('myTune5').play()"></div>
-      <div class="s6" onclick="document.getElementById('myTune6').play()"></div>
+      <div class="s1" onclick="playSound(sounds.soundOne)"></div>
+      <div class="s2" onclick="playSound(sounds.soundTwo)"></div>
+      <div class="s3" onclick="playSound(sounds.soundThree)"></div>
+      <div class="s4" onclick="playSound(sounds.soundFour)"></div>
+      <div class="s5" onclick="playSound(sounds.soundFive)"></div>
+      <div class="s6" onclick="playSound(sounds.soundSix)"></div>
     </div>
     `;
 }
+const sounds = {
+  soundOne: document.getElementById("myTune1"),
+  soundTwo: document.getElementById("myTune2"),
+  soundThree: document.getElementById("myTune3"),
+  soundFour: document.getElementById("myTune4"),
+  soundFive: document.getElementById("myTune5"),
+  soundSix: document.getElementById("myTune6"),
+};
+
 // controller
+function playSound(sound) {
+  sound.play();
+}
